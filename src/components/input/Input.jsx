6c954d75cab55ecd7f0497fa82f04e8420ec2d0d
byronjvh <byronj.v.h.123@gmail.com>
2023-05-29 20:22:19 -0600

    fix inputfield

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,7 +1,7 @@
 import { useContext, useEffect } from 'react'
 import { globalContext } from '../../context/GlobalContext'
 import './Input.css'
-import InputField from './inputField'
+import InputField from './inputField.jsx'
 
 function Input () {
   const { gameState, currentFocus, incorrect, resetIncorrect } = useContext(globalContext)
